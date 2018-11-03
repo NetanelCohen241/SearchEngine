0abc698bbd3303baaa59de9da504ec9252e1234f
DanielTwito <Daniel.twito7@gmail.com>
2018-11-04 00:41:08 +0200

    add calc size methode

--- Parse.py
+++ Parse.py
@@ -1,6 +1,9 @@
+import ast
+import re
 from _ctypes import byref
 from ctypes import c_int
 
+import math
 import nltk
 from enum import Enum
 
@@ -22,7 +25,7 @@ class Parser:
 
 
     def parse(self,text):
-        i=c_int()
+        i=0
         month = ["january", "jan", "february", "feb", "march", "mar", "april", "apr", "may", "june", "jun", "july", "jul", "august", "aug", "september", "sep", "october", "oct", "november", "nov", "december", "dec"]
         terms=[]
         tokens=nltk.word_tokenize(text)
@@ -46,14 +49,72 @@ class Parser:
 
             i=i+1
 
+    def str_to_number(self,num):
+        """
+        this function take a string vaule and return the numeric value
+        :param num: strin to convert
+        :return: numeric value
+        """
+        return ast.literal_eval("".join(num.split(",")))
+
 
     #TODO:Twitoo
-    def calcSize(self,tokens,i):
-        term=""
-        return i,term
+    def calcSize(self,tokens, i):
+        """
+        this methode assest the size of the number
+        :param tokens: list of tokens
+        :param i:index in the token list
+        :return: term insteade of 1,000,000 -> 1M , 1.5 million - > 1.5M 1 trillion -> 1000B
+        change the sizes to K/M/B
+        and return the last index the function work on
+        """
+        term = ""
+        fraction = " "
+        sizes = {"thousand": 1000, "million": 1000000, "billion": 1000000000, "trillion": 1000000000000}
+        x = self.str_to_number(tokens[i])
+        if i+1 < len(tokens):
+            if sizes.__contains__(tokens[i+1].lower()):
+                x = x * (sizes[tokens[i+1].lower()])
+                i = i + 1
+            elif self.isFraction(tokens[i+1]):
+                fraction += tokens[i+1]
+                i = i + 1
+        #claasify the size of the number
+        if x < sizes["thousand"]:
+            term = str(x) + fraction
+
+        elif sizes["thousand"] <= x < sizes["million"]:
+            if x % 1000 == 0:
+                term = str(int(x/sizes["thousand"]))+"K"
+            else:
+               term = str(x/sizes["thousand"]) + "K"
+
+        elif sizes["million"] <= x < sizes["billion"]:
+            if  x % sizes["million"] == 0:
+                term = str(int(x/sizes["million"]))+"M"
+            else:
+                term = str(x/sizes["million"]) + "M"
+
+        elif x >= sizes["billion"]:
+            if x % sizes["billion"] == 0:
+                term = str(int(x / sizes["billion"])) + "B"
+            else:
+                term = str((x / sizes["billion"])) + "B"
+
+        return i, term
 
 
 
+
+
+    def isFraction(self, fraction):
+        """
+        check if string is a fraction from the formst X/Y
+        :param fraction: string to chech
+        :return: True - is a fraction
+        """
+        list = fraction.split("/")
+        return list[0].isnumeric() and list[-1].isnumeric()
 
     def dateFormat(self, month, number):
         pass
--- test.py
+++ test.py
@@ -1,6 +1,21 @@
 import Reader
-
-r = Reader.ReadFile("D:\iretrival\corpus")
+import  Parse
+import pandas
+import nltk
+# r = Reader.ReadFile("D:\iretrival\corpus")
 
 # r.startAction()
-# print (nltk.word_tokenize("Twito like bottles in many size and price of 5000$"))
+x=Parse.Parser()
+# y=nltk.word_tokenize("1,000,400")
+# print(y)
+# print (x.calcSize(y,0))
+
+z= ["12.34", "1.4","100","1,000","1000","76,023,000,000" , "13,000,000","10,764","1010.98","75.4 thousand","55 thousand","55 million","34 billion",
+    "1.5 trillion","7 Trillion","0.4 billion","0.24 trillion","1.5 billion","130 3/5", "1 32/99","1000500","98000000123"]
+w=[]
+for t in z:
+    yy=nltk.word_tokenize(t)
+    i, term = x.calcSize(yy, 0)
+    w.append(term)
+df = pandas.DataFrame({"original value ":z,"calcsize value":w})
+print(df)
