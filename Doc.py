a0cf48248928d44ebc8f2f0d0d15c2a1666781d5
DanielTwito <Daniel.twito7@gmail.com>
2018-11-10 20:33:29 +0200

    fix calcprice

---
+++ Doc.py
@@ -0,0 +1,11 @@
+class Document:
+    def __init__(self,txt):
+        self.numberOfuniqeTerm=self.getNumberOfUniqeTerm(txt)
+        self.getMostFrequncyTermNumber(txt)
+        pass
+
+    def getNumberOfUniqeTerm(self, txt):
+        pass
+
+    def getMostFrequncyTermNumber(self, txt):
+        pass
